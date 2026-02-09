c27020e0e23f27a70e1a0a324c41f0e62cd65723
drestrepoj06 <drestrepoj06@gmail.com>
2026-02-09 15:40:56 -0500

    Added altitude files

--- EGM2008_surface/train_test/train.py
+++ EGM2008_surface/train_test/train.py
@@ -92,11 +92,11 @@ def main():
     print(f"Train samples: {len(train_df):,} | Val samples: {len(val_df):,}")
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    mode = "g_direct"
+    mode = "U"
     lr = 5e-3
     batch_size = 262144
     lmax = 3
-    hidden_layers = 2
+    hidden_layers = 31
     hidden_features = 8
     first_omega_0 = 20
     hidden_omega_0 = 1.0
