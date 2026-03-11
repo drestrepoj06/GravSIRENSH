480018a1683cf73b8a2e0c8d18d5963c1b9e40c1
drestrepoj06 <drestrepoj06@gmail.com>
2026-03-11 14:42:48 -0500

    Refining code for final submission

--- train_test/data_modules.py
+++ train_test/data_modules.py
@@ -66,15 +66,15 @@ class SplitDataset(pl.LightningDataModule):
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size,
-                          shuffle=True, num_workers=8,
+                          shuffle=True, num_workers=2,
                           pin_memory=torch.cuda.is_available(), persistent_workers=True)
 
     def val_dataloader(self):
         return DataLoader(self.val_dataset, batch_size=self.batch_size,
-                          shuffle=False, num_workers=8,
+                          shuffle=False, num_workers=2,
                           pin_memory=torch.cuda.is_available(), persistent_workers=True)
 
     def test_dataloader(self):
         return DataLoader(self.test_dataset, batch_size=self.batch_size,
-                          shuffle=False, num_workers=8,
+                          shuffle=False, num_workers=2,
                           pin_memory=torch.cuda.is_available(), persistent_workers=True)
